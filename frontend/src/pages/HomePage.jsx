d2d7521da86d3e0cbd418c057b2b8367e3af3d15
Sujal Dudhe <sujaldudhe2004@gmail.com>
2025-07-28 00:15:16 +0530

    initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { Container, Text, VStack, SimpleGrid } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
+
+const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={30}
+          fontWeight="bold"
+          bgGradient="linear(to-r, cyan.400, blue.500)"
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={6}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        {products.length === 0 && (
+          <Text
+            fontSize="xl"
+            textAlign={"center"}
+            fontWeight="bold"
+            color="gray.500"
+          >
+            No Products found ☹️{" "}
+            <Link to={"/create"}>
+              <Text
+                as="span"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create a Product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;
